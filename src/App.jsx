8bdfe580539bef5996e17995ff45ecc59d7d5280
Nijat Ismayilov <nicatismayilov43@gmail.com>
2020-10-19 17:54:23 +0400

    small changes to the code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+
+const App = () => {
+	return (
+		<div className='app'>
+			<div>
+				<Link to='/projects'>Projects</Link>
+			</div>
+
+			<Switch>
+				<Route exact path='/' component={() => <div>Dashboard</div>} />
+				<Route exact path='/projects' component={() => <div>projects</div>} />
+			</Switch>
+		</div>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<BrowserRouter>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</BrowserRouter>,
 	document.getElementById("root")
 );
