3297d92c8830e791f595cc16fde9d8fb723e51fb
Nijat ISmayilov <nicatismayilov43@gmail.com>
2020-10-21 22:38:11 +0400

    MS-9: Writing auth and user services

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,19 @@
 import axios from "axios";
+import { getToken } from "utils/sessionStorage";
 
 const axiosInstance = axios.create({
-	baseURL: "https://jsonplaceholder.typicode.com/posts",
+	baseURL: "http://localhost:5000/api",
+	responseType: "json",
+	headers: {
+		"Content-type": "application/json",
+	},
+});
+
+axiosInstance.interceptors.request.use((req) => {
+	let token = getToken();
+	req.headers.Authorization = `Bearer ${token}`;
+
+	return req;
 });
 
 export default axiosInstance;
