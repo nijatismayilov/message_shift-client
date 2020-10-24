7b28e1a6688b29973887e579529fb116889ac176
Nijat ISmayilov <nicatismayilov43@gmail.com>
2020-10-24 12:35:59 +0400

    HotFix: removing redux dev tools from production

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 
@@ -8,10 +8,16 @@ import { getToken as getTokenFromLocal } from "utils/localStorage";
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
 
+const composeEnhancers =
+	(process.env.NODE_ENV === "development" &&
+		typeof window !== "undefined" &&
+		composeWithDevTools) ||
+	compose;
+
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 sagaMiddleware.run(rootSaga);
 
